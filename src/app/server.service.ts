3ddadc0bfc27d114401a88c41e0d65f7850b929a
Leon Grin <41086608+leongrin@users.noreply.github.com>
2019-02-18 17:51:32 -0300

    service push

---
+++ src/app/server.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import {Http, Headers} from '@angular/http';
+import {catchError, map} from 'rxjs/operators';
+import {throwError} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServerService {
+
+  constructor(private http: Http) { }
+
+  storeServers(servers: any[]) {
+    const headers = new Headers({'Content-Type': 'application/json'});
+/*    return this.http.post(
+      'https://udemy-ng-http-dc6f2.firebaseio.com/data.json',
+      servers,
+      {headers: headers});*/
+    return this.http.put(
+      'https://udemy-ng-http-dc6f2.firebaseio.com/data.json',
+      servers,
+      {headers: headers});
+}
+
+  getServers() {
+    return this.http.get('https://udemy-ng-http-dc6f2.firebaseio.com/data.json').pipe(map(
+      (response) => {
+        const data = response.json();
+        return data;
+      }
+    ));
+/*      .pipe(catchError(error => {
+      console.log(error);
+      return throwError('Something went wrong');
+    });*/
+  }
+
+  getAppName() {
+    return this.http.get('https://udemy-ng-http-dc6f2.firebaseio.com/appName.json').pipe(map(
+      response => response.json()
+    ));
+  }
+
+}
+
+
