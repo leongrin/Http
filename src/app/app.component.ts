504de7eb150da040b93a779b3651656d1b912afc
Leon Grin <41086608+leongrin@users.noreply.github.com>
2019-02-18 17:50:55 -0300

    lecture 237 to 250

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
+import {ServerService} from './server.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   servers = [
     {
       name: 'Testserver',
@@ -18,6 +20,13 @@ export class AppComponent {
       id: this.generateId()
     }
   ];
+
+  appName = this.serverServ.getAppName();
+
+  httpSubscription: Subscription;
+
+  constructor(private serverServ: ServerService) {}
+
   onAddServer(name: string) {
     this.servers.push({
       name: name,
@@ -27,5 +36,23 @@ export class AppComponent {
   }
   private generateId() {
     return Math.round(Math.random() * 10000);
+  }
+
+  onSave() {
+    this.httpSubscription = this.serverServ.storeServers(this.servers).subscribe(
+      (response) => console.log(response),
+      (error) => console.log(error)
+  );
+  }
+
+  onGet() {
+    this.serverServ.getServers().subscribe(
+      (servers) => console.log(servers),
+      (error) => console.log(error)
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.httpSubscription.unsubscribe();
   }
 }
